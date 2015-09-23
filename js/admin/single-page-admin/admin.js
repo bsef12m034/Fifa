c2ad9742bc5a0393afb097abc50fada5ec84aa24
bsef12m034 <bsef12m034@pucit.edu.pk>
2015-09-23 15:28:39 +0500

    First design

---
+++ js/admin/single-page-admin/admin.js
@@ -0,0 +1,9 @@
+
+
+/* ---------- Notifications ---------- */
+	$('.noty').click(function(e){
+		e.preventDefault();
+		var options = $.parseJSON($(this).attr('data-noty-options'));
+		noty(options);
+	});
+
